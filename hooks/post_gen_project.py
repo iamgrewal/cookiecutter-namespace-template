6ae2f33d9a2e5e6212abc1bbb0bf45daa67c913b
Jatinder Grewal <8136556+iamgrewal@users.noreply.github.com>
2024-10-11 14:38:46 -0400

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python
 import os
 
-
 project_path = os.path.realpath(os.path.curdir)
 project_slug = os.path.split(project_path)[1]
 namespace = project_slug.split(".")[0]
 package_name = project_slug.split(".")[1]
 package_path = os.path.join(project_path, namespace, package_name)
 
-
-def remove_project_file(filepath):
-    os.remove(os.path.join(project_path, filepath))
-
-
-def remove_package_file(filepath):
-    os.remove(os.path.join(package_path, filepath))
-
+def remove_file(filepath):   
+    """Removes a file if it exists."""
+    if os.path.exists(filepath):
+        os.remove(filepath)
 
 if __name__ == "__main__":
     if "{{ cookiecutter.create_author_file }}" != "y":
-        remove_project_file("AUTHORS.rst")
-        remove_project_file("docs/authors.rst")
+        remove_file(os.path.join(project_path, "AUTHORS.rst"))
+        remove_file(os.path.join(project_path, "docs/authors.rst"))
 
     if "{{ cookiecutter.use_pytest }}" == "y":
-        init_file = os.path.join(project_path, "tests", "__init__.py")
-        remove_project_file(init_file)
+        remove_file(os.path.join(project_path, "tests", "__init__.py"))
 
     if "no" in "{{ cookiecutter.command_line_interface|lower }}":
-        cli_file = os.path.join(package_path, "cli.py")
-        remove_package_file(cli_file)
+        remove_file(os.path.join(package_path, "cli.py"))
 
     if "Other/Proprietary License" == "{{ cookiecutter.license }}":
-        remove_project_file("LICENSE")
+        remove_file(os.path.join(project_path, "LICENSE"))
+
+    # Additional cleanup (optional):
+    # You can add more cleanup tasks here if needed, for example:
+    # - Remove specific files based on other cookiecutter variables
+    # - Rename files
+    # - Create new directories
